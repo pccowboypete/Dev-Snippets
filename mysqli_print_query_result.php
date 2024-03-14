2cc2441e80d0c19997f219d84617a223f99bac89
Peter Nanayon <pccowboyphils@gmail.com>
2024-03-14 11:43:32 +0800

    Update mysqli_print_query_result.php

--- mysqli_print_query_result.php
+++ mysqli_print_query_result.php
@@ -1,6 +1,6 @@
 <?php
 
-  $xquery = $yourqueryhere;
+  $xquery = mysqli_query($con, "SELECT affiliate_id FROM wp_kma_affiliates LIMIT 1");
   
   while($row = mysqli_fetch_assoc($xquery)){
     foreach($row as $cname => $cvalue){
